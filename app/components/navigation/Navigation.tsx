51eac6f42431ce5cd32f2e6312cb60a2b9fb4acf
rise1997 <rising.web.ink@gmail.com>
2023-11-13 10:13:39 +0900

    サイト名変更

--- app/components/navigation/Navigation.tsx
+++ app/components/navigation/Navigation.tsx
@@ -12,10 +12,10 @@ type NavigationProps = {
 // ナビゲーション
 const Navigation: React.FC<NavigationProps> = ({ currentUser }) => {
   return (
-    <header className="shadow-lg shadow-gray-100">
+    <header className="shadow-lg shadow-gray-100 px-2">
       <div className="container mx-auto flex max-w-screen-sm items-center justify-between px-1 py-5">
-        <Link href="/" className="cursor-pointer text-xl font-bold">
-          FullStackChannel
+        <Link href="/" className="cursor-pointer  text-base md:text-lg font-semibold">
+          プログラミングレッスン
         </Link>
 
         <div className="flex items-center justify-center space-x-2">
